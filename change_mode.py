c8ad8909daef779414945118d54de9831f1df1b2
user <user@name.com>
2025-04-10 09:01:00 -0400

    hotfix

--- change_mode.py
+++ change_mode.py
@@ -1,44 +1,17 @@
 from pymavlink import mavutil
 
-main_mode_mapping_px4 = {
-    'MANUAL': 0,
-    'ALTCTL': 1,
-    'POSCTL': 2,
-    'AUTO': 3,
-    'ACRO': 4,
-    'OFFBOARD': 5,
-    'STABILIZED': 6,
-    'RATTITUDE': 7,
-}
-
-sub_mode_mapping_px4 = {
-    'READY': 0,
-    'TAKEOFF': 1,
-    'HOLD': 2,  # LOITER in MAVLink
-    'MISSION': 3,
-    'RETURN_TO_LAUNCH': 4,
-    'LAND': 5,
-    'FOLLOW_ME': 6,
-}
-
 
 def change_mode(master, mode, autopilot='ardupilot', sub_mode='NONE'):
 
-    if autopilot == 'ardupilot':
-        # Check if mode is available
-        if mode not in master.mode_mapping():
-            print(f'Unknown mode : {mode}')
-            print(f"available modes: {list(master.mode_mapping().keys())}")
-            raise Exception('Unknown mode')
-        
-        # Get mode ID
-        mode_id = master.mode_mapping()[mode]
-        sub_mode = 0
-    elif autopilot == 'px4':
-        # Get mode ID
-        mode_id = main_mode_mapping_px4[mode]
-        sub_mode = sub_mode_mapping_px4[sub_mode]
-
+    # Check if mode is available
+    if mode not in master.mode_mapping():
+        print(f'Unknown mode : {mode}')
+        print(f"available modes: {list(master.mode_mapping().keys())}")
+        raise Exception('Unknown mode')
+    
+    # Get mode ID
+    mode_id = master.mode_mapping()[mode]
+    sub_mode = 0
 
     master.mav.command_long_send(master.target_system, master.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MODE,
                                 0, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, mode_id, sub_mode, 0, 0, 0, 0)
