0c6679567ac404a3b94489c53ecf120c54b56b3b
Pi Zero 2 w <zero.2@gmail.com>
2025-04-13 13:31:46 +0100

    cleanup

--- Mission.py
+++ Mission.py
@@ -1,8 +1,12 @@
+print("Started mission")
+print("Importing modules...")
 import Scan
 import takeoff
 import time
 from pymavlink import mavutil
+print("Imports complete!")
 
+print("Waiting for hardware connection")
 the_connection = mavutil.mavlink_connection('/dev/serial0', baud=57600)
 the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % 
